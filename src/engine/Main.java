f6d6d2ee628039e1a41bcd0d45471e28b9b15835
Sam <slownie16@gmail.com>
2019-11-03 09:41:04 -0500

    attack method implemented but stuff is wrong

--- src/engine/Main.java
+++ src/engine/Main.java
@@ -2,7 +2,7 @@ package engine;
 
 //Java Imports:
 import java.util.Scanner;
-import java.util.ArrayList;
+//import java.util.ArrayList;
 
 //Engine Imports:
 import engine.PartyMember;
@@ -24,42 +24,89 @@ public class Main
         boolean phase = true;
 
         //Player Variables:
-
         //You can edit these, but don't do something stupid
         String name1 = "Player";
-        int stats1 [] = {}; //see EngineNotes.txt for array references
+        int stats1 [] = {20, 12, 8, 6, 4, 4, 2}; //see EngineNotes.txt for array references
 
+        //Enemy Variables:
+        //Maybe give the enemies a different variable name?
+        String name2 = "Enemy";
+        int stats2 [] = {9, 2, 9, 0, 2, 4, 1}; //see EngineNotes.txt for array references
+
+        //Party Member deceleration:
         PartyMember p1 = new PartyMember(name1, stats1);
-        ArrayList<PartyMember> party = new ArrayList<PartyMember>();
+
+        //Enemy deceleration:
+        Enemy e1 = new Enemy(name2, stats2);
+
+        //Unused for now, will add them when we have multiple party members and enemies:
+        //ArrayList<PartyMember> party = new ArrayList<PartyMember>();
+        //ArrayList<Enemy> enemies = new ArrayList<Enemy>();
 
         //Main Engine Loop:
         while (true)
         {
-            int command = sc.nextInt(); //don't know if I should declare it here but w/e
-
-            switch(command)
+            if (phase)
             {
-                //Attack:
-                case 1:
-                    break;
+                //Player Phase:
+                System.out.println("Turn: "+turnNumber);
+                System.out.println("Player Phase");
+                System.out.println("["+p1.name+" HP: "+p1.stats[0]+"]\n["+e1.name+" HP: "+e1.stats[0]+"]");
+                System.out.println("1) Attack 2) Skills 3) Items 4) Escape\nEnter a Command:");
+                int command = sc.nextInt(); //don't know if I should declare it here but w/e
 
-                //Skills:
-                case 2:
-                    break;
+                switch(command)
+                {
+                    //Attack:
+                    case 1:
+                        attack(p1.name, p1.stats, e1.name, e1.stats);
+                        phase = false; //Changes to enemy phase
+                        break;
 
-                //Items:
-                case 3:
-                    break;
+                    //Skills:
+                    case 2:
+                        break;
 
-                //Escape:
-                case 4:
-                    break;
+                    //Items:
+                    case 3:
+                        break;
 
-                //This shouldn't happen, but if it does here's the code:
-                default:
-                    System.out.println("Error: Invalid Command");
-                    break;
+                    //Escape:
+                    case 4:
+                        break;
+
+                    //This shouldn't happen, but if it does here's the code:
+                    default:
+                        System.out.println("Error: Invalid Command");
+                        break;
+                }
+            } else {
+                //Enemy Phase:
+                System.out.println("Enemy Phase");
+                //brain dead ai intesifies:
+                attack(e1.name, e1.stats, p1.name, p1.stats);
+                turnNumber++;
+                phase = true; //Changes to player phase
             }
+
         }
     }
+
+    //Standard attack method:
+    static void attack(String aName, int aStats[], String bName, int bStats[])
+    {
+        //Damage Calculations:
+        int damage = aStats[2] -= bStats[5]; //damage calculation = a.atk -b.def.
+        if (damage < 0) damage = 0; //prevents damage from being 0.
+        bStats[0] -= damage; //changes b.hp based on a's damage
+        if (bStats[0] < 0) bStats[0] = 0; //prevents b.hp from being 0.
+
+        //This will be a lot cleaner in the GUI version:
+        System.out.println("-----------------------"); //used to seperate battle logs
+        System.out.println(aName+" attacks "+bName+"!");
+        System.out.println(aName+" deals "+damage+" damage to "+bName);
+        System.out.println(bName+" has "+bStats[0]+" HP left.");
+        System.out.println("-----------------------"); //used to seperate battle logs
+    }
 }
+
